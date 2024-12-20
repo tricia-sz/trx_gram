73a8b2b3f44a0b10536df3adcdd514b94194231d
Patricia Souza <patricia.sz.br@gmail.com>
2024-12-20 06:21:07 -0300

    Definindo os tipos

---
+++ types/Comment.ts
@@ -0,0 +1,14 @@
+import { Post } from "./Post";
+import { User } from "./User";
+
+
+export interface Comment {
+  id: string;
+  userId: string;
+  postId: string;
+  content: string;
+  createdAt: Date;
+  updatedAt: Date;
+  user: User;
+  post?: Post;
+}
