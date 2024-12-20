8db59ac02742100151a2e3561ae3720a1891bf60
Patricia Souza <patricia.sz.br@gmail.com>
2024-12-20 06:45:52 -0300

    Mapeando os providers

--- auth.ts
+++ auth.ts
@@ -6,6 +6,8 @@ import google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
+import type { Provider } from "next-auth/providers";
+
 const prisma = new PrismaClient();
 
 const config = {
@@ -13,16 +15,27 @@ const config = {
   session: { strategy: "jwt" },
   providers: [google],
   callbacks: {
-    authorized({ request, auth }) {
-      const { pathname } = request.nextUrl;
-
-      if (pathname === "/middleware") {
-        return !!auth;
-      }
-
-      return true;
+    session({ session, token }) {
+      // Adiciona o ID do usuário à sessão
+      if (token.sub) session.user.userId = token.sub;
+      return session;
     },
+  },
+  pages: {
+    signIn: "/signin",
   },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
+
+// Definir o tipo do provedor
+interface ProviderWithId {
+  id: string;
+  name: string;
+}
+
+// Mapeando os provedores manualmente
+export const providerMap = config.providers.map((provider) => {
+  const typedProvider = provider as unknown as ProviderWithId;
+  return { id: typedProvider.id, name: typedProvider.name };
+});
